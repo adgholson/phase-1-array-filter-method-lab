18e0c6553136a17955f4f12a712e2ed882433696
Aaron <adgholson98@gmail.com>
2023-08-03 11:03:42 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, name){
+    const lowerCaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseName);
+}
+
+function fuzzyMatch(drivers, beginningLetters){
+    const lowerCaseBeginning = beginningLetters.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseBeginning));
+}
+
+function matchName(drivers, name){
+    return drivers.filter((driver) => driver.name === name);
+}
